e966396337b593a40d19ef7bb9a5e05bbe7b609c
prajjwal3 <prajjwal@khojdeal.com>
2023-08-09 14:21:44 +0530

    ds

---
+++ app/api/summarize/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+    let imageURL = await request.json()
+    const url = `https://image-caption-generator2.p.rapidapi.com/v2/captions?imageUrl=${imageURL}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'b7a19997eemsh935ad0881fafed8p14a019jsn6a0880a9fa8e',
+            'X-RapidAPI-Host': 'image-caption-generator2.p.rapidapi.com'
+        }
+    };
+
+  try {
+    const response = await fetch(url, options);
+    let data = await response.json();
+    console.log(response);
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ msg: "error" }, { status: 200 });
+  }
+}
